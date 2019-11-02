128c1786595b7432623d034343357b7324faee69
Maykol HL <hmaykol@gmail.com>
2019-11-02 01:00:38 +0100

    Fix server startup

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-'use strict';
+var AlexaAppServer = require('alexa-app-server');
 
-var AlexaServer = require( 'alexa-app-server' );
+var instance = AlexaAppServer.start({
+  server_root: __dirname,     // Path to root
+  public_html: "public_html", // Static content
+  app_dir: "apps",            // Location of alexa-app modules
+  app_root: "/alexa/",        // Service root
+  port: 8080                  // Port to use
+});
 
-var server = new AlexaServer( {
- httpsEnabled: false,
- port: process.env.PORT || 8080
-} );
-
-server.start();
+//instance.stop();              // Stop the server
